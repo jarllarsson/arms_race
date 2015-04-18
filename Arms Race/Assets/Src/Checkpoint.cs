badd02cd7120c9d662c36bb15dcb1c54dcbd69ba
Jarl Larsson <jarl.larsson@gmail.com>
2015-04-18 17:02:04 +0200

    checkpoints

---
+++ Arms Race/Assets/Src/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+    public int m_id;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int getId()
+    {
+        return m_id;
+    }
+}
---
+++ Arms Race/Assets/Src/CheckpointChecker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointChecker : MonoBehaviour 
+{
+    public int m_currentCheckpointId;
+    public int m_lap;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider p_object)
+    {
+        if (p_object.tag=="checkpoint")
+        {
+            Checkpoint chkPoint = p_object.gameObject.GetComponent<Checkpoint>();
+            if (chkPoint!=null)
+            {
+                int chkPointId = chkPoint.getId();
+                if (m_currentCheckpointId == chkPointId-1) // current id must be the checkpoint before this one
+                {
+                    m_currentCheckpointId = chkPointId;
+                }
+            }
+        }
+        if (p_object.tag == "goal" && m_currentCheckpointId!=0)
+        {
+            m_currentCheckpointId = 0;
+            m_lap++;
+        }
+    }
+}

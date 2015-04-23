db6601511b33a6bb707fc030ad5c3d227649d966
Jarl Larsson <jarl.larsson@gmail.com>
2015-04-23 11:51:30 +0200

    multiplayer screen compositions

--- Arms Race/Assets/Src/InputCarController.cs
+++ Arms Race/Assets/Src/InputCarController.cs
@@ -6,30 +6,33 @@ public class InputCarController : MonoBehaviour
     public CarController m_carController;
     public CarAttack m_atk;
     private bool m_released = true;
+    public int m_playerControllerId = 0;
+    private string m_controllerIdStr;
 
 	// Use this for initialization
 	void Start () 
     {
-	
+        m_playerControllerId = Mathf.Clamp(m_playerControllerId, 0,4);
+        m_controllerIdStr = "P" + m_playerControllerId.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        m_carController.ThrustPedal(Input.GetAxis("Thrust"));
-        m_carController.SteeringWheel(Input.GetAxis("Horizontal"));
+        m_carController.ThrustPedal(Input.GetAxis(m_controllerIdStr + "Thrust"));
+        m_carController.SteeringWheel(Input.GetAxis(m_controllerIdStr + "Horizontal"));
 
-        if (Input.GetAxis("FireRight") > 0.0f && m_released)
+        if (Input.GetAxis(m_controllerIdStr+"FireRight") > 0.0f && m_released)
         {
             m_atk.FireRight();
             m_released = false;
         }
-        if (Input.GetAxis("FireLeft") > 0.0f && m_released)
+        if (Input.GetAxis(m_controllerIdStr + "FireLeft") > 0.0f && m_released)
         {
             m_atk.FireLeft();
             m_released = false;
         }
-        if (Input.GetAxis("FireRight") < 0.1f && Input.GetAxis("FireLeft") < 0.1f)
+        if (Input.GetAxis(m_controllerIdStr + "FireRight") < 0.1f && Input.GetAxis(m_controllerIdStr + "FireLeft") < 0.1f)
         {
             m_released = true;
         }
---
+++ Arms Race/Assets/Src/PlayerHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHandler : MonoBehaviour 
+{
+    public GameObject[] m_players;
+    public bool[] m_playersActive;
+	// Use this for initialization
+	void Awake() 
+    {
+	    for(int i=0;i<m_players.Length;i++)
+        {
+            if (i<m_playersActive.Length && !m_playersActive[i])
+            {
+                DestroyImmediate(m_players[i]);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
